a4d469dc476c4d227578856a2c01099ce378f783
Ellie <doris0707@naver.com>
2022-09-10 04:36:04 +0900

    [feat] view

--- backend/letters/views.py
+++ backend/letters/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
+from .models import letter, anniversary
+from users.models import user
 # Create your views here.
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from letters.models import letter, anniversary
+from .models import user
 
 # Create your views here.
