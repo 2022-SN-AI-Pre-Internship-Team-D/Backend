6ff450830cce5128d6eb3ed414fb887c589ec4a8
Ellie <doris0707@naver.com>
2022-09-09 19:40:31 +0900

    [build] url settings

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,5 +19,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/users/', admin.site.urls),
-    path('api/v1/letters', admin.site.urls),
+    path('api/v1/letters/', admin.site.urls),
 ]
