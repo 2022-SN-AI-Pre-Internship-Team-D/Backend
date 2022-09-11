c114beb3105cc7cae2c57bff75a3b8f07c5a4a76
Ellie <doris0707@naver.com>
2022-09-11 18:59:12 +0900

    [feat] add username to token

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin', admin.site.urls),
-    path('api/v1/users', include('users.urls')),
-    path('api/v1/letters', include('letters.urls')),
+    path('admin/', admin.site.urls),
+    path('api/v1/users/', include('users.urls')),
+    path('api/v1/letters/', include('letters.urls')),
 ]
