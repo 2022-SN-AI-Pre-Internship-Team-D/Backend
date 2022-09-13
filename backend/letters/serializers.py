3c8974baa15d4e21286c0ca5440ed9e5e79b4a55
Ellie <doris0707@naver.com>
2022-09-13 22:51:43 +0900

    [feat] letter count serializer

--- backend/letters/serializers.py
+++ backend/letters/serializers.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
 from .models import letter, anniversary
 
-class letterSerializer(serializers.ModelSerializer):
+class LetterSerializer(serializers.ModelSerializer):
     class Meta:
         model = letter
         fields = ("id", "uuid", "user_id", "anni_id", "text", "file", "is_active")
+
+class EventLetterStatisticsSerializer(serializers.Serializer):
+    event = serializers.SerializerMethodField()
+    count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = letter
+        fields = '__all__'
+
+    def get_kind(self, model_instance):
+        return model_instance['event']
+
+    def get_cnt(self, model_instance):
+        return model_instance['count']
 
