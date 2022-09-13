574c639d5dae3666c24948ce48d1e68b7db72e15
Ellie <doris0707@naver.com>
2022-09-13 19:53:46 +0900

    [feat]letter serializer

--- backend/letters/serializers.py
+++ backend/letters/serializers.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from .models import letter, anniversary
+
+class letterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = letter
+        fields = ("id", "uuid", "user_id", "anni_id", "text", "file", "is_active")
