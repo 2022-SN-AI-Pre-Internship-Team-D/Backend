5fa3401545e9570a4722f8eb41b428291da50f16
zyoon <develop_z@naver.com>
2022-09-15 00:29:15 +0900

    [feat] sign-up

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 import uuid
 from django.contrib.auth.models import AbstractUser
+from datetime import date, datetime
 
 class User(AbstractUser):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     email = models.EmailField(blank=True, unique=True)
+    birth = models.DateField(default=datetime.now)
     is_active = models.IntegerField(default=1)
 
     USERNAME_FIELD = 'email'
@@ -12,4 +14,3 @@ class User(AbstractUser):
 
     class Meta:
         db_table = 'user'
-
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from letters.models import letter, anniversary
 from .models import User
+from .serializers import SignupSirializer
 
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from rest_framework import generics, status
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+class SignupView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = SignupSirializer
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
@@ -25,6 +29,7 @@ class MyTokenObtainPairView(TokenObtainPairView):
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
+        'sign-up/',
         'sign-in/',
         'token/refresh/',
         'token/verify/',
