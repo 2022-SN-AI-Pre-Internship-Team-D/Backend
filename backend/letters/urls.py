072d09fc00d702ee1a3220f93c6c2e25a974eb02
Ellie <doris0707@naver.com>
2022-09-14 22:37:43 +0900

    [feat] edit anni model

--- backend/letters/urls.py
+++ backend/letters/urls.py
@@ -7,6 +7,6 @@ urlpatterns =[
     path('users/<user_uuid>/events/all/counts',views.get_all_events_cnt),
     path('users/<user_uuid>/events/<event_uuid>/counts',views.get_event_cnt),
     path('events/<event_uuid>/check-date',views.check_date),
-    path('users/<user_uuid>/events/birthday/check-birth-date',views.check-birth-date),
+    path('users/<user_uuid>/events/birthday/check-birth-date',views.check_birth_date),
 
 ]
