ee7aaaf3f04dbe0756fc1ee78f8d79677492b432
zyoon <develop_z@naver.com>
2022-09-20 02:06:41 +0900

    [feat] serializer-1

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -4,6 +4,7 @@ from .models import User
 from users.models import User
 from letters import utils
 from .serializers import *
+from uuid import uuid4
 
 from django.http import JsonResponse
 from django.contrib.auth import get_user_model
@@ -28,11 +29,19 @@ class SignupSirializer(serializers.ModelSerializer):
         required=True
     )
     image = serializers.FileField(
-        required=True
+        required=True,
+        # use_url = True
     )
+
+    uuid = str(uuid4())
+    image = utils.get_file_url(image, uuid)
+    # User.objects.create(uuid=uuid, user_id=user, text=text, file=file_url, media=media)
     
+    # print(image)
+
     class Meta:
         model = User
+        image = serializers.CharField()
         fields = ('username','email','password','password2','birth','image')
     
     def validate(self, data):
@@ -42,24 +51,31 @@ class SignupSirializer(serializers.ModelSerializer):
             })
         
         return data
+# post로 로그인할때 다른 정보받아오고 파일 보내는 부분은 
+# meta image필드 재정의! get image한다음에 
 
     def create(self, validated_data):
         user = User.objects.create(
             username = validated_data['username'],
             email = validated_data['email'],
-            birth = validated_data['birth']
+            birth = validated_data['birth'],
+            # image = validated_data['image']
         )
+        # uuid = str(uuid4())
+
         user.image = validated_data['image']
+        # user.image = validated_data['image_url']
+        # user.image = utils.get_file_url(image, uuid)
+        
+        # image = utils.get_file_url(image, uuid)
+        # user.objects.create(uuid=uuid, user_id=user, image=image)
+
         token = RefreshToken.for_user(user)
         user.set_password(validated_data['password'])
         user.refreshtoken = token
         user.save()
     
         return user
-
-class SignupView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = SignupSirializer
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
